65b2adbcd2ba465bdae2b7fec9e9124e8ef8c75f
Yousif Abdulhussein <abdulhusseinyousifh@gmail.com>
2026-01-23 15:46:21 -0500

    Init shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="h-full flex">
+    <aside class="p-6 w-56 flex flex-col justify-between">
+        <h1 class="font-brand text-4xl leading-tighter">Impart</h1>
+
+        <nav>
+            <p>Solutions</p>
+            <p>Resources</p>
+            <p>Customer Hub</p>
+        </nav>
+
+        <button class="p-3 text-sm bg-black text-white font-light rounded-sm">
+            Book a Demo
+        </button>
+    </aside>
+
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</main>
