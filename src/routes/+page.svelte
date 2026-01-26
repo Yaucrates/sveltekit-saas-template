5493cbd6421eff3aac53322b42ef3c21525f6ee2
Yousif Abdulhussein <abdulhusseinyousifh@gmail.com>
2026-01-26 01:27:56 -0500

    Sidebar complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <main class="h-full flex">
-    <aside class="p-6 w-56 flex flex-col justify-between">
+    <aside class="px-8 py-6 w-56 flex flex-col justify-between">
         <h1 class="font-brand text-4xl leading-tighter">Impart</h1>
 
-        <nav>
-            <p>Solutions</p>
-            <p>Resources</p>
-            <p>Customer Hub</p>
+        <nav class="flex flex-col gap-10">
+            <div class="gap-3 flex flex-col text-sm">
+                <a href="/">Resources</a>
+                <a href="/">Sign In</a>
+            </div>
+
+            <div class="gap-3 flex flex-col text-xs">
+                <p class="text-neutral-400 uppercase">Platform</p>
+                <a href="/#curriculum" class="text-neutral-500">Course Curriculum</a>
+                <a href="/#problem-sets" class="text-neutral-500">Problem Sets</a>
+                <a href="/#automated-grading" class="text-neutral-500">Automated Grading</a>
+                <a href="/#student-analytics" class="text-neutral-500">Student Analytics</a>
+            </div>
         </nav>
 
         <button class="p-3 text-sm bg-black text-white font-light rounded-sm">
